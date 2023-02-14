0d958352d549eb85c095b1b72955b6d7a0de0133
SANTIAGO RUIZ LONDOÑO <santiagoruiz247667@correo.itm.edu.co>
2023-02-14 16:33:26 -0500

    Referencias_Inicio

---
+++ SolucionSistemaVenta/SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.Intrinsics.X86;
+using SistemaVenta.DBContext;
+//using SistemaVenta.DAL.Implementacion;
+//using SistemaVenta.DAL.Interfaces;
+//using SistemaVenta.BLL.Implementacion;
+//using SistemaVenta.BLL.interfaces;
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencia
+    {
+
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration Configuration) {
+
+            services.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("CadenaSQL"));
+            });
+        
+        }
+    }
+}
